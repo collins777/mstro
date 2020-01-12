c868fb6b8677cfcd0761af6fa0c1498492bbda4c
Brandon Collins <brandoncollins@Brandons-iMac.local>
2020-01-12 00:25:13 -0500

    Added state to the SearchForm component allowing form submission values to be saved

--- src/components/search-form/search-form.component.jsx
+++ src/components/search-form/search-form.component.jsx
@@ -2,9 +2,45 @@ import React from "react";
 import "./search-form.styles.scss";
 
 class SearchForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      searchInput: "",
+      sortBy: "",
+      limit: ""
+    };
+  }
+
+  handleInputChange = e => {
+    // Sets the value of this.state.searchInput
+    this.setState({
+      searchInput: e.target.value
+    });
+  };
+
+  handleSortChange = e => {
+    this.setState({
+      sortBy: e.target.value
+    });
+  };
+
+  handleLimitChange = e => {
+    this.setState({
+      limit: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    alert(
+      `The search input value is ${this.state.searchInput} ${this.state.sortBy} ${this.state.limit}`
+    );
+  };
+
   render() {
     return (
-      <form id="search-form">
+      <form id="search-form" onSubmit={this.handleSubmit}>
         <h1>Hello Search</h1>
         {/* Search Input */}
         <input
@@ -13,6 +49,8 @@ class SearchForm extends React.Component {
           type="text"
           name="name"
           placeholder="Search Topic.."
+          onChange={this.handleInputChange}
+          value={this.state.searchInput}
           required
         />
 
@@ -23,7 +61,8 @@ class SearchForm extends React.Component {
             className="select-input"
             name="sortby"
             id="sortby"
-            placeholder="Sort by"
+            onChange={this.handleSortChange}
+            value={this.state.sortBy}
           >
             <option selected disabled hidden>
               Sort By
@@ -35,7 +74,13 @@ class SearchForm extends React.Component {
 
         {/* Dropdown for Limiting # of results*/}
         <div className="limit-container">
-          <select className="select-input" name="limit" id="limit">
+          <select
+            className="select-input"
+            name="limit"
+            id="limit"
+            onChange={this.handleLimitChange}
+            value={this.state.limit}
+          >
             <option selected disabled hidden>
               Limit Results
             </option>
